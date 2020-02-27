f52a17d0e58b77a559649ba65a76717b0811125e
Nico Andrade <nicolas.andrade@ssense.com>
2020-02-27 13:57:35 -0500

    Socket hangup

--- socket-hang-up/code/server.js
+++ socket-hang-up/code/server.js
@@ -3,10 +3,22 @@ const http = require('http')
 const slowDown = process.env.SLOWDOWN_MS || 200
 const hostname = '0.0.0.0'
 const port = 3000
+const multiplier = 9999;
 
 const server = http.createServer((req, res) => {
   console.log("Request being handled ...")
+  if (req.url.endsWith('memory')) {
 
+    const obj = {}
+
+    for(let i = 0; i < multiplier; i++){
+      const arr = new Array(multiplier).fill(multiplier);
+      obj[i] = JSON.parse(JSON.stringify(arr));
+    }
+    console.log("Using memory");
+    res.end(JSON.stringify(obj) + '\n');
+    return ;
+  }
 
   if (req.url.endsWith('destroy')) {
     console.log("DESTROY THIS SOCKET")
@@ -27,17 +39,21 @@ const server = http.createServer((req, res) => {
 })
 
 
+setInterval(() => {
+  const memoryUsage = process.memoryUsage();
+  console.log(Object.keys(memoryUsage).reduce((p, c) => { 
+    p[c] = p[c] / 1024 / 1024; 
+    return p;}, memoryUsage));
+}, 5000)
+
 server.on('close', () => {
   console.log("Connection closed.")
 })
 server.on('connection', (s) => {
-
   console.log('>> Request')
   // s.end() // Half-closes the socket. i.e., it sends a FIN packet. It is possible the server will still send some data.
   // s.destroy() // Ensures that no more I/O activity happens on this socket. Only necessary in case of errors (parse error or so).
   // s.write('EEE');
-
-
 })
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`)
